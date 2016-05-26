b153203eb1139e9a3e493d3a8785d4904b44c459
sfali23 <f.syed.ali@gmail.com>
2016-05-26 15:43:06 -0400

    initializing TransformerModule

--- src/main/java/com/alphasystem/app/sarfengine/guice/GuiceSupport.java
+++ src/main/java/com/alphasystem/app/sarfengine/guice/GuiceSupport.java
@@ -3,6 +3,8 @@ package com.alphasystem.app.sarfengine.guice;
 import com.alphasystem.app.sarfengine.conjugation.builder.ConjugationBuilderFactory;
 import com.alphasystem.app.sarfengine.conjugation.member.MemberBuilderFactory;
 import com.alphasystem.app.sarfengine.conjugation.rule.RuleProcessorFactory;
+import com.alphasystem.app.sarfengine.conjugation.transformer.noun.TransformerFactory;
+import com.alphasystem.app.sarfengine.conjugation.transformer.noun.TransformerModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.mycila.guice.ext.closeable.CloseableModule;
@@ -11,7 +13,7 @@ import com.mycila.guice.ext.jsr250.Jsr250Module;
 /**
  * @author sali
  */
-public class GuiceSupport {
+public final class GuiceSupport {
 
     private static GuiceSupport instance = new GuiceSupport();
     private final Injector injector;
@@ -20,8 +22,8 @@ public class GuiceSupport {
      * Do not let anyone instantiate this class
      */
     private GuiceSupport() {
-        injector = Guice.createInjector(new CloseableModule(), new Jsr250Module(), new RuleProcessorModule(),
-                new MemberBuilderModule(), new ConjugationBuilderModule());
+        injector = Guice.createInjector(new CloseableModule(), new Jsr250Module(), new TransformerModule(),
+                new RuleProcessorModule(), new MemberBuilderModule(), new ConjugationBuilderModule());
     }
 
     public static GuiceSupport getInstance() {
@@ -30,6 +32,10 @@ public class GuiceSupport {
 
     public Injector getInjector() {
         return injector;
+    }
+
+    public TransformerFactory getTransformerFactory() {
+        return injector.getInstance(TransformerFactory.class);
     }
 
     public RuleProcessorFactory getRuleProcessorFactory() {
