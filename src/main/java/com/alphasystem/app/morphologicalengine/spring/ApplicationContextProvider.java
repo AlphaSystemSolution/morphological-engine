fe01ada5c1527f6ef6d6bc6e0904e576336f5aa2
sfali23 <f.syed.ali@gmail.com>
2017-03-10 11:57:05 -0500

    added method to get transformers and other

--- src/main/java/com/alphasystem/app/morphologicalengine/spring/ApplicationContextProvider.java
+++ src/main/java/com/alphasystem/app/morphologicalengine/spring/ApplicationContextProvider.java
@@ -1,5 +1,18 @@
 package com.alphasystem.app.morphologicalengine.spring;
 
+import com.alphasystem.app.morphologicalengine.conjugation.builder.FormBuilder;
+import com.alphasystem.app.morphologicalengine.conjugation.rule.RuleInfo;
+import com.alphasystem.app.morphologicalengine.conjugation.rule.RuleProcessor;
+import com.alphasystem.app.morphologicalengine.conjugation.rule.RuleProcessorFactory;
+import com.alphasystem.app.morphologicalengine.conjugation.rule.RuleProcessorType;
+import com.alphasystem.app.morphologicalengine.conjugation.transformer.factory.noun.NounTransformerFactory;
+import com.alphasystem.app.morphologicalengine.conjugation.transformer.factory.noun.NounTransformerFactoryType;
+import com.alphasystem.app.morphologicalengine.conjugation.transformer.factory.verb.VerbTransformerFactory;
+import com.alphasystem.app.morphologicalengine.conjugation.transformer.factory.verb.VerbTransformerFactoryType;
+import com.alphasystem.app.morphologicalengine.conjugation.transformer.noun.NounTransformer;
+import com.alphasystem.app.morphologicalengine.conjugation.transformer.noun.NounTransformerType;
+import com.alphasystem.app.morphologicalengine.conjugation.transformer.verb.VerbTransformer;
+import com.alphasystem.app.morphologicalengine.conjugation.transformer.verb.VerbTransformerType;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
@@ -17,6 +30,31 @@ public class ApplicationContextProvider implements ApplicationContextAware {
 
     public static <T> T getBean(String name, Class<T> type) {
         return applicationContext.getBean(name, type);
+    }
+
+    public static FormBuilder getFormBuilder() {
+        return getBean(FormBuilder.class);
+    }
+
+    public static VerbTransformerFactory getVerbTransformerFactory(VerbTransformerFactoryType.Type type) {
+        return getBean(type.name(), VerbTransformerFactory.class);
+    }
+
+    public static NounTransformerFactory getNounTransformerFactory(NounTransformerFactoryType.Type type) {
+        return getBean(type.name(), NounTransformerFactory.class);
+    }
+
+    public static VerbTransformer getVerbTransformer(VerbTransformerType.Type type) {
+        return getBean(type.name(), VerbTransformer.class);
+    }
+
+    public static NounTransformer getNounTransformer(NounTransformerType.Type type) {
+        return getBean(type.name(), NounTransformer.class);
+    }
+
+    public static RuleProcessor getRuleProcessor(RuleProcessorType.Type type, RuleInfo ruleInfo) {
+        final RuleProcessorFactory ruleProcessorFactory = getBean(type.name(), RuleProcessorFactory.class);
+        return ruleProcessorFactory.createRuleProcessor(ruleInfo);
     }
 
     @Override
