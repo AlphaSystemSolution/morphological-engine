a6906fad93e6d785ef86b0eb125d313e7dffdc2a
sfali23 <f.syed.ali@gmail.com>
2017-04-17 13:48:59 -0400

    clean up

--- src/main/java/com/alphasystem/app/morphologicalengine/conjugation/builder/ConjugationBuilderHelper.java
+++ src/main/java/com/alphasystem/app/morphologicalengine/conjugation/builder/ConjugationBuilderHelper.java
@@ -1,6 +1,5 @@
 package com.alphasystem.app.morphologicalengine.conjugation.builder;
 
-import org.apache.commons.lang3.ArrayUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -33,18 +32,8 @@ import com.alphasystem.morphologicalengine.model.NounDetailedConjugationPair;
 import com.alphasystem.morphologicalengine.model.VerbConjugationGroup;
 import com.alphasystem.morphologicalengine.model.VerbDetailedConjugationPair;
 
-import static com.alphasystem.arabic.model.ArabicLetterType.AIN;
-import static com.alphasystem.arabic.model.ArabicLetterType.ALIF;
-import static com.alphasystem.arabic.model.ArabicLetterType.ALIF_HAMZA_ABOVE;
-import static com.alphasystem.arabic.model.ArabicLetterType.DTHA;
-import static com.alphasystem.arabic.model.ArabicLetterType.FA;
-import static com.alphasystem.arabic.model.ArabicLetterType.HA;
-import static com.alphasystem.arabic.model.ArabicLetterType.LAM;
-import static com.alphasystem.arabic.model.ArabicLetterType.MEEM;
 import static com.alphasystem.arabic.model.ArabicLetterType.NOON;
-import static com.alphasystem.arabic.model.ArabicLetterType.RA;
 import static com.alphasystem.arabic.model.ArabicLetterType.WAW;
-import static com.alphasystem.arabic.model.ArabicLetterType.YA;
 import static com.alphasystem.arabic.model.ArabicLetterType.ZAIN;
 import static com.alphasystem.arabic.model.ArabicWord.getWord;
 
@@ -56,12 +45,6 @@ final class ConjugationBuilderHelper {
     private static final Logger LOGGER = LoggerFactory.getLogger(ConjugationBuilder.class);
 
     private static final ArabicWord WEIGHT_LABEL = getWord(WAW, ZAIN, NOON);
-    private static final ArabicWord PARTICIPLE_PREFIX = getWord(FA, HA, WAW);
-    private static final ArabicWord COMMAND_PREFIX = getWord(ALIF, LAM, ALIF_HAMZA_ABOVE, MEEM, RA, ArabicLetterType.SPACE,
-            MEEM, NOON, HA);
-    private static final ArabicWord FORBIDDING_PREFIX = getWord(WAW, NOON, HA, YA, ArabicLetterType.SPACE, AIN, NOON, HA);
-    private static final ArabicWord NOUN_OF_PLACE_AND_TIME_PREFIX = getWord(WAW, ALIF, LAM, DTHA, RA, FA, ArabicLetterType.SPACE, MEEM,
-            NOON, HA);
 
     static ConjugationHeader createConjugationHeader(ConjugationRoots conjugationRoots, String pastTenseRoot,
                                                      String presentTenseRoot,
@@ -140,21 +123,6 @@ final class ConjugationBuilderHelper {
             builder.append(" ").append(getStringValue(ArabicLetterType.LEFT_PARENTHESIS, outputFormat))
                     .append(getStringValue(rootLetters.toLabel(), outputFormat))
                     .append(getStringValue(ArabicLetterType.RIGHT_PARENTHESIS, outputFormat));
-        }
-        return builder.toString();
-    }
-
-    private static String toDefaultStringValue(ArabicWord prefix, OutputFormat outputFormat, String... values) {
-        if (ArrayUtils.isEmpty(values)) {
-            return null;
-        }
-        StringBuilder builder = new StringBuilder();
-        builder.append(values[0]);
-        for (int i = 1; i < values.length; i++) {
-            builder.append(" ").append(getStringValue(ArabicLetterType.WAW, outputFormat)).append(" ").append(values[i]);
-        }
-        if (prefix != null && builder.length() > 0) {
-            builder.insert(0, String.format("%s ", getStringValue(prefix, outputFormat)));
         }
         return builder.toString();
     }
