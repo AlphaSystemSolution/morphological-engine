d574c25741cc50b92f0fd734f82538199b66d8e2
sfali23 <f.syed.ali@gmail.com>
2017-04-30 15:36:58 -0400

    Using AbbreviatedRecord

--- src/main/java/com/alphasystem/app/morphologicalengine/conjugation/builder/AbbreviatedConjugationBuilder.java
+++ src/main/java/com/alphasystem/app/morphologicalengine/conjugation/builder/AbbreviatedConjugationBuilder.java
@@ -15,6 +15,7 @@ import com.alphasystem.morphologicalanalysis.morphology.model.ConjugationConfigu
 import com.alphasystem.morphologicalanalysis.morphology.model.RootLetters;
 import com.alphasystem.morphologicalanalysis.morphology.model.support.SarfTermType;
 import com.alphasystem.morphologicalengine.model.AbbreviatedConjugation;
+import com.alphasystem.morphologicalengine.model.AbbreviatedRecord;
 
 import static com.alphasystem.app.morphologicalengine.conjugation.builder.ConjugationBuilder.checkFourthRadical;
 import static com.alphasystem.app.morphologicalengine.conjugation.builder.ConjugationBuilderHelper.createDefaultNoun;
@@ -40,100 +41,101 @@ import static java.lang.String.format;
  */
 public class AbbreviatedConjugationBuilder {
 
-    public AbbreviatedConjugation doAbbreviatedConjugation(final ConjugationRoots conjugationRoots) {
-        return doAbbreviatedConjugation(conjugationRoots, OutputFormat.UNICODE);
-    }
+	public AbbreviatedConjugation doAbbreviatedConjugation(final ConjugationRoots conjugationRoots) {
+		return doAbbreviatedConjugation(conjugationRoots, OutputFormat.UNICODE);
+	}
 
-    public AbbreviatedConjugation doAbbreviatedConjugation(final ConjugationRoots conjugationRoots,
-                                                           final OutputFormat outputFormat) {
-        RootLetters rootLetters = conjugationRoots.getRootLetters();
-        checkFourthRadical(rootLetters);
+	public AbbreviatedConjugation doAbbreviatedConjugation(final ConjugationRoots conjugationRoots,
+														   final OutputFormat outputFormat) {
+		RootLetters rootLetters = conjugationRoots.getRootLetters();
+		checkFourthRadical(rootLetters);
 
-        final ConjugationConfiguration conjugationConfiguration = conjugationRoots.getConjugationConfiguration();
-        final NamedTemplate template = conjugationRoots.getTemplate();
-        final RuleInfo ruleInfo = new RuleInfo(template, rootLetters,
-                conjugationConfiguration.isSkipRuleProcessing());
-        final RuleProcessor ruleProcessor = getRuleProcessor(RULE_ENGINE, ruleInfo);
+		final ConjugationConfiguration conjugationConfiguration = conjugationRoots.getConjugationConfiguration();
+		final NamedTemplate template = conjugationRoots.getTemplate();
+		final RuleInfo ruleInfo = new RuleInfo(template, rootLetters,
+				conjugationConfiguration.isSkipRuleProcessing());
+		final RuleProcessor ruleProcessor = getRuleProcessor(RULE_ENGINE, ruleInfo);
 
-        final boolean removePassiveLine = conjugationConfiguration.isRemovePassiveLine() ||
-                (conjugationRoots.getPastPassiveTense() == null);
+		final boolean removePassiveLine = conjugationConfiguration.isRemovePassiveLine() ||
+				(conjugationRoots.getPastPassiveTense() == null);
 
-        final AbbreviatedConjugation abbreviatedConjugation = new AbbreviatedConjugation();
-        abbreviatedConjugation.setId(format("%s_%s", template.name(), rootLetters.getName()));
+		final AbbreviatedConjugation abbreviatedConjugation = new AbbreviatedConjugation();
+		abbreviatedConjugation.setId(format("%s_%s", template.name(), rootLetters.getName()));
 
-        // past tense
-        String pastTense = createDefaultVerb(PAST_TENSE, conjugationRoots.getPastTense(), ruleProcessor, rootLetters, outputFormat);
-        abbreviatedConjugation.pastTense(pastTense);
+		// past tense
+		AbbreviatedRecord pastTense = createDefaultVerb(PAST_TENSE, conjugationRoots.getPastTense(), ruleProcessor,
+				rootLetters, outputFormat);
+		abbreviatedConjugation.setPastTense(pastTense);
 
-        // present tense
-        String presentTense = createDefaultVerb(PRESENT_TENSE, conjugationRoots.getPresentTense(), ruleProcessor,
-                rootLetters, outputFormat);
-        abbreviatedConjugation.presentTense(presentTense);
+		// present tense
+		AbbreviatedRecord presentTense = createDefaultVerb(PRESENT_TENSE, conjugationRoots.getPresentTense(), ruleProcessor,
+				rootLetters, outputFormat);
+		abbreviatedConjugation.setPresentTense(presentTense);
 
-        abbreviatedConjugation.setConjugationHeader(ConjugationBuilderHelper.createConjugationHeader(conjugationRoots,
-                pastTense, presentTense, outputFormat));
+		abbreviatedConjugation.setConjugationHeader(ConjugationBuilderHelper.createConjugationHeader(conjugationRoots,
+				pastTense.getLabel(), presentTense.getLabel(), outputFormat));
 
-        // active participle masculine
-        String defaultValue = createDefaultNoun(ACTIVE_PARTICIPLE_MASCULINE, conjugationRoots.getActiveParticipleMasculine(),
-                ruleProcessor, rootLetters, outputFormat);
-        abbreviatedConjugation.activeParticipleMasculine(defaultValue);
+		// active participle masculine
+		AbbreviatedRecord defaultValue = createDefaultNoun(ACTIVE_PARTICIPLE_MASCULINE, conjugationRoots.getActiveParticipleMasculine(),
+				ruleProcessor, rootLetters, outputFormat);
+		abbreviatedConjugation.setActiveParticipleMasculine(defaultValue);
 
-        // active participle feminine
-        defaultValue = createDefaultNoun(ACTIVE_PARTICIPLE_FEMININE, conjugationRoots.getActiveParticipleFeminine(),
-                ruleProcessor, rootLetters, outputFormat);
-        abbreviatedConjugation.activeParticipleFeminine(defaultValue);
+		// active participle feminine
+		defaultValue = createDefaultNoun(ACTIVE_PARTICIPLE_FEMININE, conjugationRoots.getActiveParticipleFeminine(),
+				ruleProcessor, rootLetters, outputFormat);
+		abbreviatedConjugation.setActiveParticipleFeminine(defaultValue);
 
-        // imperative
-        defaultValue = createDefaultVerb(IMPERATIVE, conjugationRoots.getImperative(), ruleProcessor, rootLetters, outputFormat);
-        abbreviatedConjugation.imperative(defaultValue);
+		// imperative
+		defaultValue = createDefaultVerb(IMPERATIVE, conjugationRoots.getImperative(), ruleProcessor, rootLetters, outputFormat);
+		abbreviatedConjugation.setImperative(defaultValue);
 
-        // forbidden
-        defaultValue = createDefaultVerb(FORBIDDING, conjugationRoots.getForbidding(), ruleProcessor, rootLetters, outputFormat);
-        abbreviatedConjugation.forbidding(defaultValue);
+		// forbidden
+		defaultValue = createDefaultVerb(FORBIDDING, conjugationRoots.getForbidding(), ruleProcessor, rootLetters, outputFormat);
+		abbreviatedConjugation.setForbidding(defaultValue);
 
-        if (!removePassiveLine) {
-            // past passive tense
-            defaultValue = createDefaultVerb(PAST_PASSIVE_TENSE, conjugationRoots.getPastPassiveTense(), ruleProcessor,
-                    rootLetters, outputFormat);
-            abbreviatedConjugation.pastPassiveTense(defaultValue);
+		if (!removePassiveLine) {
+			// past passive tense
+			defaultValue = createDefaultVerb(PAST_PASSIVE_TENSE, conjugationRoots.getPastPassiveTense(), ruleProcessor,
+					rootLetters, outputFormat);
+			abbreviatedConjugation.setPastPassiveTense(defaultValue);
 
-            // present passive tense
-            defaultValue = createDefaultVerb(PRESENT_PASSIVE_TENSE, conjugationRoots.getPresentPassiveTense(), ruleProcessor,
-                    rootLetters, outputFormat);
-            abbreviatedConjugation.presentPassiveTense(defaultValue);
+			// present passive tense
+			defaultValue = createDefaultVerb(PRESENT_PASSIVE_TENSE, conjugationRoots.getPresentPassiveTense(), ruleProcessor,
+					rootLetters, outputFormat);
+			abbreviatedConjugation.setPresentPassiveTense(defaultValue);
 
-            // passive participle masculine
-            defaultValue = createDefaultNoun(PASSIVE_PARTICIPLE_MASCULINE, conjugationRoots.getPassiveParticipleMasculine(),
-                    ruleProcessor, rootLetters, outputFormat);
-            abbreviatedConjugation.passiveParticipleMasculine(defaultValue);
+			// passive participle masculine
+			defaultValue = createDefaultNoun(PASSIVE_PARTICIPLE_MASCULINE, conjugationRoots.getPassiveParticipleMasculine(),
+					ruleProcessor, rootLetters, outputFormat);
+			abbreviatedConjugation.setPassiveParticipleMasculine(defaultValue);
 
-            // passive participle masculine
-            defaultValue = createDefaultNoun(PASSIVE_PARTICIPLE_FEMININE, conjugationRoots.getPassiveParticipleFeminine(),
-                    ruleProcessor, rootLetters, outputFormat);
-            abbreviatedConjugation.passiveParticipleFeminine(defaultValue);
-        }
+			// passive participle masculine
+			defaultValue = createDefaultNoun(PASSIVE_PARTICIPLE_FEMININE, conjugationRoots.getPassiveParticipleFeminine(),
+					ruleProcessor, rootLetters, outputFormat);
+			abbreviatedConjugation.setPassiveParticipleFeminine(defaultValue);
+		}
 
-        // verbal nouns
-        abbreviatedConjugation.verbalNouns(createMultiValues(VERBAL_NOUN, ruleProcessor, conjugationRoots.getVerbalNouns(),
-                rootLetters, outputFormat));
+		// verbal nouns
+		abbreviatedConjugation.setVerbalNouns(createMultiValues(VERBAL_NOUN, ruleProcessor, conjugationRoots.getVerbalNouns(),
+				rootLetters, outputFormat));
 
-        abbreviatedConjugation.adverbs(createMultiValues(NOUN_OF_PLACE_AND_TIME, ruleProcessor,
-                conjugationRoots.getAdverbs(), rootLetters, outputFormat));
+		abbreviatedConjugation.setAdverbs(createMultiValues(NOUN_OF_PLACE_AND_TIME, ruleProcessor,
+				conjugationRoots.getAdverbs(), rootLetters, outputFormat));
 
 
-        return abbreviatedConjugation;
-    }
+		return abbreviatedConjugation;
+	}
 
-    private String[] createMultiValues(SarfTermType sarfTermType, RuleProcessor ruleProcessor,
-                                       NounRootBase[] nouns, RootLetters rootLetters, OutputFormat outputFormat) {
-        if (ArrayUtils.isEmpty(nouns)) {
-            return null;
-        }
-        final List<String> values = new ArrayList<>();
+	private AbbreviatedRecord[] createMultiValues(SarfTermType sarfTermType, RuleProcessor ruleProcessor,
+												  NounRootBase[] nouns, RootLetters rootLetters, OutputFormat outputFormat) {
+		if (ArrayUtils.isEmpty(nouns)) {
+			return null;
+		}
+		final List<AbbreviatedRecord> values = new ArrayList<>();
 
-        Arrays.stream(nouns).forEach(nounRootBase -> values.add(createDefaultNoun(sarfTermType, nounRootBase, ruleProcessor,
-                rootLetters, outputFormat)));
+		Arrays.stream(nouns).forEach(nounRootBase -> values.add(createDefaultNoun(sarfTermType, nounRootBase, ruleProcessor,
+				rootLetters, outputFormat)));
 
-        return values.toArray(new String[values.size()]);
-    }
+		return values.toArray(new AbbreviatedRecord[values.size()]);
+	}
 }
--- src/main/java/com/alphasystem/app/morphologicalengine/conjugation/builder/ConjugationBuilderHelper.java
+++ src/main/java/com/alphasystem/app/morphologicalengine/conjugation/builder/ConjugationBuilderHelper.java
@@ -21,7 +21,9 @@ import com.alphasystem.arabic.model.VerbType;
 import com.alphasystem.arabic.model.WeakVerbType;
 import com.alphasystem.morphologicalanalysis.morphology.model.RootLetters;
 import com.alphasystem.morphologicalanalysis.morphology.model.RootWord;
+import com.alphasystem.morphologicalanalysis.morphology.model.support.NounSupport;
 import com.alphasystem.morphologicalanalysis.morphology.model.support.SarfTermType;
+import com.alphasystem.morphologicalengine.model.AbbreviatedRecord;
 import com.alphasystem.morphologicalengine.model.ConjugationHeader;
 import com.alphasystem.morphologicalengine.model.NounConjugation;
 import com.alphasystem.morphologicalengine.model.NounConjugationGroup;
@@ -37,141 +39,155 @@ import static com.alphasystem.arabic.model.ArabicWord.getWord;
  */
 final class ConjugationBuilderHelper {
 
-    // private static final Logger LOGGER = LoggerFactory.getLogger(ConjugationBuilder.class);
+	// private static final Logger LOGGER = LoggerFactory.getLogger(ConjugationBuilder.class);
 
-    private static final ArabicWord WEIGHT_LABEL = getWord(WAW, ZAIN, NOON);
+	private static final ArabicWord WEIGHT_LABEL = getWord(WAW, ZAIN, NOON);
 
-    static ConjugationHeader createConjugationHeader(ConjugationRoots conjugationRoots, String pastTenseRoot,
-                                                     String presentTenseRoot,
-                                                     OutputFormat outputFormat) {
+	static ConjugationHeader createConjugationHeader(ConjugationRoots conjugationRoots, String pastTenseRoot,
+													 String presentTenseRoot,
+													 OutputFormat outputFormat) {
 
-        RootLetters rootLetters = conjugationRoots.getRootLetters();
-        WordStatus status = new WordStatus(rootLetters);
-        RootType rootType = RootType.CONSONANT;
-        VerbType verbType = VerbType.CONSONANT;
-        WeakVerbType weakVerbType = null;
-        if (status.isWeak()) {
-            rootType = RootType.WEAK;
-            if (status.twoSeparateLettersWeak()) {
-                verbType = VerbType.TWO_SEPARATE_RADICALS_WEAK;
-            } else if (status.twoConsecutiveLettersWeak()) {
-                verbType = VerbType.TWO_CONSECUTIVE_RADICALS_WEAK;
-            } else if (status.isAssimilated()) {
-                verbType = VerbType.FIRST_RADICAL_WEAK;
-                weakVerbType = status.isFirstRadicalWaw() ? WeakVerbType.FIRST_RADICAL_WEAK_WAW
-                        : WeakVerbType.FIRST_RADICAL_WEAK_YA;
-            } else if (status.isHollow()) {
-                verbType = VerbType.SECOND_RADICAL_WEAK;
-                weakVerbType = status.isSecondRadicalWaw() ? WeakVerbType.SECOND_RADICAL_WEAK_WAW
-                        : WeakVerbType.SECOND_RADICAL_WEAK_YA;
-            } else if (status.isDefective()) {
-                verbType = VerbType.THIRD_RADICAL_WEAK;
-                weakVerbType = status.isThirdRadicalWaw() ? WeakVerbType.THIRD_RADICAL_WEAK_WAW
-                        : WeakVerbType.THIRD_RADICAL_WEAK_YA;
-            }
-        } else if (status.isDoubledLettered()) {
-            verbType = VerbType.DOUBLE_LETTERED;
-        } else if (status.isHamzatted()) {
-            if (status.isFirstRadicalHamza()) {
-                verbType = VerbType.FIRST_RADICAL_HAMZA;
-            } else if (status.isSecondRadicalHamza()) {
-                verbType = VerbType.SECOND_RADICAL_HAMZA;
-            } else if (status.isThirdRadicalHamza()) {
-                verbType = VerbType.THIRD_RADICAL_HAMZA;
-            }
-        }
+		RootLetters rootLetters = conjugationRoots.getRootLetters();
+		WordStatus status = new WordStatus(rootLetters);
+		RootType rootType = RootType.CONSONANT;
+		VerbType verbType = VerbType.CONSONANT;
+		WeakVerbType weakVerbType = null;
+		if (status.isWeak()) {
+			rootType = RootType.WEAK;
+			if (status.twoSeparateLettersWeak()) {
+				verbType = VerbType.TWO_SEPARATE_RADICALS_WEAK;
+			} else if (status.twoConsecutiveLettersWeak()) {
+				verbType = VerbType.TWO_CONSECUTIVE_RADICALS_WEAK;
+			} else if (status.isAssimilated()) {
+				verbType = VerbType.FIRST_RADICAL_WEAK;
+				weakVerbType = status.isFirstRadicalWaw() ? WeakVerbType.FIRST_RADICAL_WEAK_WAW
+						: WeakVerbType.FIRST_RADICAL_WEAK_YA;
+			} else if (status.isHollow()) {
+				verbType = VerbType.SECOND_RADICAL_WEAK;
+				weakVerbType = status.isSecondRadicalWaw() ? WeakVerbType.SECOND_RADICAL_WEAK_WAW
+						: WeakVerbType.SECOND_RADICAL_WEAK_YA;
+			} else if (status.isDefective()) {
+				verbType = VerbType.THIRD_RADICAL_WEAK;
+				weakVerbType = status.isThirdRadicalWaw() ? WeakVerbType.THIRD_RADICAL_WEAK_WAW
+						: WeakVerbType.THIRD_RADICAL_WEAK_YA;
+			}
+		} else if (status.isDoubledLettered()) {
+			verbType = VerbType.DOUBLE_LETTERED;
+		} else if (status.isHamzatted()) {
+			if (status.isFirstRadicalHamza()) {
+				verbType = VerbType.FIRST_RADICAL_HAMZA;
+			} else if (status.isSecondRadicalHamza()) {
+				verbType = VerbType.SECOND_RADICAL_HAMZA;
+			} else if (status.isThirdRadicalHamza()) {
+				verbType = VerbType.THIRD_RADICAL_HAMZA;
+			}
+		}
 
-        final NamedTemplate template = conjugationRoots.getTemplate();
-        ChartMode chartMode = new ChartMode(template, rootType, verbType, weakVerbType);
-        ConjugationHeader conjugationHeader = new ConjugationHeader();
-        conjugationHeader.setRootLetters(rootLetters);
-        conjugationHeader.setChartMode(chartMode);
-        conjugationHeader.setBaseWord(getStringValue(template.toLabel(), outputFormat));
-        conjugationHeader.setPastTenseRoot(pastTenseRoot);
-        conjugationHeader.setPresentTenseRoot(presentTenseRoot);
-        conjugationHeader.setTranslation(conjugationRoots.getTranslation());
-        conjugationHeader.setTitle(getHeaderTitle(pastTenseRoot, presentTenseRoot, rootLetters, outputFormat));
-        conjugationHeader.setTypeLabel1(getStringValue(template.getType(), outputFormat));
-        conjugationHeader.setTypeLabel2(getStringValue(ArabicWord.concatenateWithSpace(WEIGHT_LABEL, template.toLabel()), outputFormat));
+		final NamedTemplate template = conjugationRoots.getTemplate();
+		ChartMode chartMode = new ChartMode(template, rootType, verbType, weakVerbType);
+		ConjugationHeader conjugationHeader = new ConjugationHeader();
+		conjugationHeader.setRootLetters(rootLetters);
+		conjugationHeader.setChartMode(chartMode);
+		conjugationHeader.setBaseWord(getStringValue(template.toLabel(), outputFormat));
+		conjugationHeader.setPastTenseRoot(pastTenseRoot);
+		conjugationHeader.setPresentTenseRoot(presentTenseRoot);
+		conjugationHeader.setTranslation(conjugationRoots.getTranslation());
+		conjugationHeader.setTitle(getHeaderTitle(pastTenseRoot, presentTenseRoot, rootLetters, outputFormat));
+		conjugationHeader.setTypeLabel1(getStringValue(template.getType(), outputFormat));
+		conjugationHeader.setTypeLabel2(getStringValue(ArabicWord.concatenateWithSpace(WEIGHT_LABEL, template.toLabel()), outputFormat));
 
-        ArabicWord label = verbType.getLabel();
-        if (weakVerbType != null) {
-            final ArabicWord arabicWord = ArabicWord.concatenate(label, new ArabicWord(ArabicLetters.LETTER_COMMA));
-            label = ArabicWord.concatenateWithSpace(arabicWord, weakVerbType.getLabel());
-        }
-        conjugationHeader.setTypeLabel3(getStringValue(label, outputFormat));
+		ArabicWord label = verbType.getLabel();
+		if (weakVerbType != null) {
+			final ArabicWord arabicWord = ArabicWord.concatenate(label, new ArabicWord(ArabicLetters.LETTER_COMMA));
+			label = ArabicWord.concatenateWithSpace(arabicWord, weakVerbType.getLabel());
+		}
+		conjugationHeader.setTypeLabel3(getStringValue(label, outputFormat));
 
-        return conjugationHeader;
-    }
+		return conjugationHeader;
+	}
 
-    private static String getHeaderTitle(String pastTenseRoot, String presentTenseRoot, RootLetters rootLetters,
-                                         OutputFormat outputFormat) {
-        StringBuilder builder = new StringBuilder();
-        if (pastTenseRoot != null) {
-            builder.append(pastTenseRoot).append(" ");
-        }
-        if (presentTenseRoot != null) {
-            builder.append(presentTenseRoot);
-        }
+	private static String getHeaderTitle(String pastTenseRoot, String presentTenseRoot, RootLetters rootLetters,
+										 OutputFormat outputFormat) {
+		StringBuilder builder = new StringBuilder();
+		if (pastTenseRoot != null) {
+			builder.append(pastTenseRoot).append(" ");
+		}
+		if (presentTenseRoot != null) {
+			builder.append(presentTenseRoot);
+		}
 
-        if (rootLetters != null) {
-            builder.append(" ").append(getStringValue(ArabicLetterType.LEFT_PARENTHESIS, outputFormat))
-                    .append(getStringValue(rootLetters.toLabel(), outputFormat))
-                    .append(getStringValue(ArabicLetterType.RIGHT_PARENTHESIS, outputFormat));
-        }
-        return builder.toString();
-    }
+		if (rootLetters != null) {
+			builder.append(" ").append(getStringValue(ArabicLetterType.LEFT_PARENTHESIS, outputFormat))
+					.append(getStringValue(rootLetters.toLabel(), outputFormat))
+					.append(getStringValue(ArabicLetterType.RIGHT_PARENTHESIS, outputFormat));
+		}
+		return builder.toString();
+	}
 
-    static String createDefaultVerb(SarfTermType sarfTermType, VerbRootBase verbRootBase, RuleProcessor ruleProcessor,
-                                    RootLetters rootLetters, OutputFormat outputFormat) {
-        final VerbTransformerFactory factory = MorphologicalEngineFactory.getVerbTransformerFactory(verbRootBase.getType());
-        final boolean imperativeOrForbidding = SarfTermType.IMPERATIVE.equals(sarfTermType) ||
-                SarfTermType.FORBIDDING.equals(sarfTermType);
-        final VerbTransformer verbTransformer = imperativeOrForbidding ? factory.secondPersonMasculineTransformer() :
-                factory.thirdPersonMasculineTransformer();
-        return verbTransformer.doTransform(ruleProcessor, verbRootBase.getRoot().getRootWord(), sarfTermType,
-                outputFormat, rootLetters).getSingular();
-    }
+	static AbbreviatedRecord createDefaultVerb(SarfTermType sarfTermType, VerbRootBase verbRootBase, RuleProcessor ruleProcessor,
+											   RootLetters rootLetters, OutputFormat outputFormat) {
+		final VerbTransformerFactory factory = MorphologicalEngineFactory.getVerbTransformerFactory(verbRootBase.getType());
+		final boolean imperativeOrForbidding = SarfTermType.IMPERATIVE.equals(sarfTermType) ||
+				SarfTermType.FORBIDDING.equals(sarfTermType);
+		final VerbTransformer verbTransformer = imperativeOrForbidding ? factory.secondPersonMasculineTransformer() :
+				factory.thirdPersonMasculineTransformer();
+		final String value = verbTransformer.doTransform(ruleProcessor, verbRootBase.getRoot().getRootWord(), sarfTermType,
+				outputFormat, rootLetters).getSingular();
+		AbbreviatedRecord abbreviatedRecord = new AbbreviatedRecord();
+		abbreviatedRecord.setId(verbRootBase.getId());
+		abbreviatedRecord.setType(sarfTermType);
+		abbreviatedRecord.setLabel(value);
+		abbreviatedRecord.setSource(verbRootBase.getRoot().name());
+		return abbreviatedRecord;
+	}
 
-    static String createDefaultNoun(SarfTermType sarfTermType, NounRootBase nounRootBase, RuleProcessor ruleProcessor,
-                                    RootLetters rootLetters, OutputFormat outputFormat) {
-        final NounTransformerFactory factory = MorphologicalEngineFactory.getNounTransformerFactory(nounRootBase.getType());
-        final NounTransformer nounTransformer = factory.singularTransformer();
-        final RootWord rootWord = nounRootBase.getSingularBaseWord().getRootWord();
-        final NounConjugation nounConjugation = nounTransformer.doTransform(ruleProcessor, rootWord, sarfTermType,
-                outputFormat, rootLetters);
-        return SarfTermType.VERBAL_NOUN.equals(sarfTermType) ? nounConjugation.getAccusative() : nounConjugation.getNominative();
-    }
+	static AbbreviatedRecord createDefaultNoun(SarfTermType sarfTermType, NounRootBase nounRootBase, RuleProcessor ruleProcessor,
+											   RootLetters rootLetters, OutputFormat outputFormat) {
+		final NounTransformerFactory factory = MorphologicalEngineFactory.getNounTransformerFactory(nounRootBase.getType());
+		final NounTransformer nounTransformer = factory.singularTransformer();
+		final NounSupport singularBaseWord = nounRootBase.getSingularBaseWord();
+		final RootWord rootWord = singularBaseWord.getRootWord();
+		final NounConjugation nounConjugation = nounTransformer.doTransform(ruleProcessor, rootWord, sarfTermType,
+				outputFormat, rootLetters);
+		final String value = SarfTermType.VERBAL_NOUN.equals(sarfTermType) ? nounConjugation.getAccusative() :
+				nounConjugation.getNominative();
+		AbbreviatedRecord abbreviatedRecord = new AbbreviatedRecord();
+		abbreviatedRecord.setId(nounRootBase.getId());
+		abbreviatedRecord.setType(sarfTermType);
+		abbreviatedRecord.setLabel(value);
+		abbreviatedRecord.setSource(singularBaseWord.name());
+		return abbreviatedRecord;
+	}
 
-    static VerbConjugationGroup createVerbConjugationGroup(SarfTermType sarfTermType, VerbRootBase verbRootBase, RuleProcessor ruleProcessor,
-                                                           RootLetters rootLetters, OutputFormat outputFormat) {
-        final VerbTransformerFactory factory = MorphologicalEngineFactory.getVerbTransformerFactory(verbRootBase.getType());
-        return factory.doConjugation(ruleProcessor, sarfTermType, outputFormat, verbRootBase, rootLetters);
-    }
+	static VerbConjugationGroup createVerbConjugationGroup(SarfTermType sarfTermType, VerbRootBase verbRootBase, RuleProcessor ruleProcessor,
+														   RootLetters rootLetters, OutputFormat outputFormat) {
+		final VerbTransformerFactory factory = MorphologicalEngineFactory.getVerbTransformerFactory(verbRootBase.getType());
+		return factory.doConjugation(ruleProcessor, sarfTermType, outputFormat, verbRootBase, rootLetters);
+	}
 
-    static NounConjugationGroup createNounConjugationGroup(SarfTermType sarfTermType, NounRootBase nounRootBase, RuleProcessor ruleProcessor,
-                                                           RootLetters rootLetters, OutputFormat outputFormat) {
-        final NounTransformerFactory factory = MorphologicalEngineFactory.getNounTransformerFactory(nounRootBase.getType());
-        return factory.doConjugation(ruleProcessor, sarfTermType, outputFormat, nounRootBase, rootLetters);
-    }
+	static NounConjugationGroup createNounConjugationGroup(SarfTermType sarfTermType, NounRootBase nounRootBase, RuleProcessor ruleProcessor,
+														   RootLetters rootLetters, OutputFormat outputFormat) {
+		final NounTransformerFactory factory = MorphologicalEngineFactory.getNounTransformerFactory(nounRootBase.getType());
+		return factory.doConjugation(ruleProcessor, sarfTermType, outputFormat, nounRootBase, rootLetters);
+	}
 
-    private static String getStringValue(ArabicSupport arabicSupport, OutputFormat outputFormat) {
-        if (arabicSupport == null) {
-            return null;
-        }
-        String value;
-        final ArabicWord arabicWord = arabicSupport.toLabel();
-        switch (outputFormat) {
-            case HTML:
-                value = arabicWord.toHtmlCode();
-                break;
-            case BUCK_WALTER:
-                value = arabicWord.toBuckWalter();
-                break;
-            default:
-                value = arabicWord.toUnicode();
-                break;
-        }
-        return value;
-    }
+	private static String getStringValue(ArabicSupport arabicSupport, OutputFormat outputFormat) {
+		if (arabicSupport == null) {
+			return null;
+		}
+		String value;
+		final ArabicWord arabicWord = arabicSupport.toLabel();
+		switch (outputFormat) {
+			case HTML:
+				value = arabicWord.toHtmlCode();
+				break;
+			case BUCK_WALTER:
+				value = arabicWord.toBuckWalter();
+				break;
+			default:
+				value = arabicWord.toUnicode();
+				break;
+		}
+		return value;
+	}
 }
